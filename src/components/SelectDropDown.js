0482c75fb1953b4d298b21ed627b7c2fbf317776
Chaostradamus <71539742+Chaostradamus@users.noreply.github.com>
2022-04-16 18:55:53 -0400

    final

--- src/components/SelectDropDown.js
+++ src/components/SelectDropDown.js
@@ -1,12 +1,12 @@
-const SelectDropDown = () => {
+const SelectDropDown = ({ style, setShowModal, selectedLanguage }) => {
   return (
-    <div className={"select-drop-down"}>
-      <input />
+    <div onClick={() => setShowModal(style)} className="select-drop-down">
+      <input value={selectedLanguage} />
       <div className="down-arrow">
         <svg
           focusable="false"
           xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 24 25"
+          viewBox="0 0 24 24"
         >
           <path d="M7 10l5 5 5-5z"></path>
         </svg>
--- src/components/TextBox.js
+++ src/components/TextBox.js
@@ -1,13 +1,37 @@
 import SelectDropDown from "./SelectDropDown";
 
-const TextBox = ({ style }) => {
+const TextBox = ({
+  style,
+  setShowModal,
+  selectedLanguage,
+  setTextToTranslate,
+  textToTranslate,
+  translatedText,
+  setTranslatedText,
+}) => {
+  const handleClick = () => {
+    setTextToTranslate("");
+    setTranslatedText("");
+  };
   return (
     <div className={style}>
-      <SelectDropDown />
-      <textarea
-        placeholder={style == "input" ? "Enter Text" : "Translation"}
-        disabled={style == "output"}
+      <SelectDropDown
+        style={style}
+        setShowModal={setShowModal}
+        selectedLanguage={selectedLanguage}
       />
+      <textarea
+        disabled={style === "output"}
+        className={style}
+        placeholder={style == "input" ? "Enter text" : "Translation"}
+        onChange={(e) => setTextToTranslate(e.target.value)}
+        value={style === "input" ? textToTranslate : translatedText}
+      />
+      {style === "input" && (
+        <div className="delete" onClick={handleClick}>
+          ˟
+        </div>
+      )}
     </div>
   );
 };
