1df26d64c841a533f2dbbceae7a973a8768dc0f8
Chaostradamus <71539742+Chaostradamus@users.noreply.github.com>
2022-04-16 16:37:22 -0400

    1st commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import TextBox from "./components/TextBox";
+import Arrows from "./components/Arrows";
+import Button from "./components/Button";
+import Modal from "./components/Modal";
 
-function App() {
+const App = () => {
+  const [inputLanguage, setInputLanguage] = useState("English");
+  const [outputLanguage, setOutputLanguage] = useState("Polish");
+  const handleClick = () => {
+    setInputLanguage(setOutputLanguage);
+    setOutputLanguage(setInputLanguage);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <TextBox style={"input"} />
+      <div className="arrow-container" onClick={handleClick}>
+        <Arrows />
+      </div>
+      <TextBox style={"output"} />
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/components/TextBox.js
@@ -0,0 +1,15 @@
+import SelectDropDown from "./SelectDropDown";
+
+const TextBox = ({ style }) => {
+  return (
+    <div className={style}>
+      <SelectDropDown />
+      <textarea
+        placeholder={style == "input" ? "Enter Text" : "Translation"}
+        disabled={style == "output"}
+      />
+    </div>
+  );
+};
+
+export default TextBox;
